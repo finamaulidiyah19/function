042b60a28c341b75b6812e06d949458b4539128e
finamaulidiyah19 <56226660+finamaulidiyah19@users.noreply.github.com>
2019-10-21 08:36:38 +0700

    Add files via upload

---
+++ Fina.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int jumlah (int x, int y){
+    return x+y;
+}
+
+int main(){
+    int a=5, b=6, c=8, d=11;
+    cout<<"a + b = "<<jumlah(a,b)<<endl;
+    cout<<"c + d = "<<jumlah(c,d);
+}
+
